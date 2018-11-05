8f029740b2c80431ed3a3429eb5fa4a2b3b9d9f4
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-11-05 19:31:18 -0300

    Update repository

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @comments = Comment.all
+    @comments = @post.comments.all
   end
 
   # GET /posts/new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+
+10.times do |i|
+  u = User.new(email:"email#{i}@gmail.com", name:"nombre #{i}", password:'123456', password_confirmation:'123456')
+  u.save
+  p = Post.new(title: "post #{i}", content: "contenido #{i}", user: u)
+  p.save
+  10.times do |e|
+    c = p.comments.build(content: "Comentario #{e} del post #{i}", user: u, post: p)
+    c.save
+  end
+end
